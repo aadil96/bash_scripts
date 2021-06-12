e3135bad0d73bb5370deab3ce076444be4e55885
aadil96 <agwan96@gmail.com>
2021-06-12 14:45:59 +0530

    add: laravel setup script for linux

---
+++ laravel_setup.sh
@@ -0,0 +1,15 @@
+#linux
+
+sudo apt update
+
+sudo apt install wget curl php php-common -y
+
+sudo apt install composer -y
+
+sudo apt update
+
+composer require laravel/installer
+
+mkdir ~/Developement/Laravel
+
+cd ~/Developement/Laravel
